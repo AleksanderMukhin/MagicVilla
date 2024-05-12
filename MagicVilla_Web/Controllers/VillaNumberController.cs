a24b179d031e409a8d9f9e202e78e1ddb8193c10
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2024-05-12 20:15:42 +0200

    create number

--- MagicVilla_Web/Controllers/VillaNumberController.cs
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -1,8 +1,10 @@
 using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models.VM;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers;
@@ -10,10 +12,12 @@ namespace MagicVilla_Web.Controllers;
 public class VillaNumberController : Controller
 {
     private readonly IVillaNumberServices _villaNumberServices;
+    private readonly IVillaServices _villaServices;
     private readonly IMapper _mapper;
     // GET
-    public VillaNumberController(IVillaNumberServices villaNumberServices, IMapper mapper)
+    public VillaNumberController(IVillaServices villaServices, IVillaNumberServices villaNumberServices, IMapper mapper)
     {
+        _villaServices = villaServices;
         _villaNumberServices = villaNumberServices;
         _mapper = mapper;
     }
@@ -28,5 +32,38 @@ public class VillaNumberController : Controller
             list = JsonConvert.DeserializeObject<List<VillaNomberDTO>>(Convert.ToString(response.Result));
         }
         return View(list);
+    }
+
+    public async Task<IActionResult> CreateVillaNumber()
+    {
+        VillaNumberCreateVM villaNumberVM = new();
+        var response = await _villaServices.GetAllAsync<APIResponse>();
+        if (response != null && response.IsSuccess)
+        {
+            villaNumberVM.VillaList = JsonConvert.DeserializeObject<List<VillaDto>>
+                (Convert.ToString(response.Result)).Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+        }
+
+        return View(villaNumberVM);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> CreateVillaNumber(VillaNumberCreateVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            var response = await _villaNumberServices.CreateAsync<APIResponse>(model.VillaNumber);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(IndexVillaNumber));
+            }
+        }
+        
+        return View(model);
     }
 }
